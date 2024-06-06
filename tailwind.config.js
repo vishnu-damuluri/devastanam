bb7bffa7123baff165f65506dff46343b2d736c3
vishnu <vishnu.damuluri@gmail.com>
2024-06-06 13:10:49 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js}",
+    "./views/*.ejs"
+  ],
+  theme: {
+    extend: {
+      screens: {
+        phone: { min: "360px", max: "640px" },
+        ipad: { min: "768px", max: "1024px" },
+        pc: { min: "1280px", max: "7680px" },
+      },
+      colors: {
+        clifford: "#da373d",
+      },
+    },
+  },
+  plugins: [],
+};
